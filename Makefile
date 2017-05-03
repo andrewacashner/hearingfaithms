fdd0050c939e823ac87d3bd4f4398a8254fb49c8
Andrew A. Cashner <andrewacashner@gmail.com>
2017-05-03 09:29:47 -0700

    true Makefile

---
+++ Makefile
@@ -0,0 +1,6 @@
+all: 
+	latexmk -outdir=aux -bibtex -pdf main
+	cp aux/main.pdf ./
+
+clean:
+	rm main.pdf aux/*.* aux/*/*.* 
--- cleanup.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env sh
-
-rm -i *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.loe *.lof *.lom *.lop \
-      *.lot *.run.xml *.toc aux/*
-
-rm -i chapters/*.aux
