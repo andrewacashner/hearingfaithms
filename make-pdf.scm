6902345258367bdae6431e266ac2eb8cd1859014
Andrew A. Cashner <andrewacashner@gmail.com>
2018-04-03 07:57:07 -0400

    trying to set up "pipeline" between sub-programs

--- make-pdf.scm
+++ make-pdf.scm
@@ -1,6 +1,7 @@
 (use-modules
   (ice-9 format)
   (xref xref)
+  (xref xref-list)
   (shell filenames)
   (shell readwrite))
 
@@ -20,22 +21,37 @@
       (system (format #f "pandoc ~a -o ~a ~a" 
                       pandoc-options-pdf outfile infile)))))
 
-(define pdf-header-inputs
+(define pdf-input-before
   '("config/pdf.yaml"
     "chapters/head.yaml"
     "chapters/copyright.md"))
 
-(define make-crossrefs
-  (lambda (chapter-ls)
-    (let* ([infiles (append pdf-header-inputs 
-                            chapter-ls
-                           '("chapters/floats.md"))]
-           [text (file-ls->string infiles)]
-           [tmp (open-output-string)])
-      (begin
-        (display text tmp)
-        (let ([tmp (open-input-string (get-output-string tmp))]) 
-          (do-xref (list tmp)))))))
+(define pdf-input-after
+  '("chapters/floats.md"))
 
-;; maybe you need a pipe!
+(define make-crossrefs
+  (lambda (chapter-ls auxfile outfile)
+    (let* ([infiles (append pdf-input-before 
+                            chapter-ls 
+                            pdf-input-after)]
+      [text (file-ls->string infiles)])
+    (do-xref text auxfile outfile)))) 
+; avoid outfile?
+
+(define do-make-pdf
+  (lambda chapter-ls
+    (begin 
+      (make-crossrefs chapter-ls "xref.aux" "tmp.md")
+      (let* ([filenames (xref-list "xref.aux" "config/xref-labels.scm")]
+             [text (file-ls->string filenames)])
+        (do-xref text "xref.aux" "tmp2.md"))
+      (join-files "tmp.md" "tmp2.md") ; instead append from do-xref?
+      (md2pdf "tmp.pdf")))) ; add outfile name
+
+
+
+;; xref-list make list of filenames from auxfile
+;; input float files from list to xref
+;; xref all together and append text to tmp
+;; run pandoc on tmp -> output-pdf
 
