84cf6718c860f6936f282eab2b5c99f64ca4ab54
Andrew A. Cashner <andrewacashner@gmail.com>
2018-04-09 09:06:04 -0400

    Documentation comments added to make-text.scm, some clarifying reorganization

--- scripts/make-text.scm
+++ scripts/make-text.scm
@@ -36,13 +36,16 @@
 --bibliography master.bib \
 --csl chicago-fullnote-bibliography.csl \
 ")
- 
+
 (define pandoc-cmd
   (lambda (opts outfile)
+    "Construct pandoc invocation with options and output file"
     (format #f "pandoc ~a -o ~a" opts outfile)))
 
 (define pandoc-pipe
   (lambda (str opts outfile)
+    "Create pipe calling pandoc with OPTS and OUTFILe, write STR to pipe and
+    then close pipe"
     (let* ([pandoc-str (pandoc-cmd opts outfile)]
            [pandoc-port (open-output-pipe pandoc-str)]
            [do-pandoc (display str pandoc-port)])
@@ -52,6 +55,8 @@
  
 (define files->pandoc
   (lambda (type labels ls outfile)
+    "Take LS list of files, alist of LABELS, and use pandoc to convert files to
+    format TYPE ('pdf or 'odt) in OUTFILE"
     (let* ([text (file-ls->string ls)]
            [text (replace-refs text labels)]
            [text (replace-labels text)]
@@ -63,6 +68,7 @@
 
 (define touch-file
   (lambda (file)
+    "Create dummy file"
       (call-with-output-file 
         file 
         (lambda (port) 
@@ -70,29 +76,29 @@
 
 (define make-labels
   (lambda (files ls)
+    "Read FILES and make alist LS of labels found"
     (let* ([text (file-ls->string files)])
       (list-labels text ls))))
            
 (define md->pdf
   (lambda (chapters)
+    "Convert list of markdown files to PDF using pandoc, with cross-references
+    resolved"
     (let* ([outfile     "pdf/all.pdf"]
-
            [headers     '("config/pdf.yaml" "chapters/head.yaml")]
            [backmatter  '("chapters/floats.md")]
-
-           [config      "config/xref-labels.scm"]
+           ; config determines which labels are filenames
+           [config      "config/xref-labels.scm"] 
            [config      (call-with-input-file config read)]
-
            ; need input.ent or latexmk won't work
            [endnotefile "input.ent"]
            [endnotes    (touch-file endnotefile)]
-
+           ; Get label list from files
            [files       (append headers chapters backmatter)]
            [labels      (make-labels files '())]
-
+           ; Get list of float files from label list and add to text
            [floatfiles  (labels->files labels config)]
            [allfiles    (append files floatfiles)])
-
       (begin
         (files->pandoc 'pdf labels allfiles outfile)
         (delete-file endnotefile)
@@ -110,28 +116,30 @@
 
 (define md->odt
   (lambda (chapters)
+    "Convert list of markdown files to separate ODT files per chapter using
+    pandoc, with cross-references resolved"
     (let* ([headers '("config/odt.yaml" "chapters/head.yaml")] 
            [backmatter '("chapters/floats.md")]
-
            [config "config/xref-labels.scm"]
            [config (call-with-input-file config read)]
-
            [endnotefile "input.ent"]
            [endnotes (touch-file endnotefile)]
-
+           ; Get labels from full text of all chapters
            [files-all (append headers chapters backmatter)]
            [labels (make-labels files-all '())]
-
+           ; Assemble floats from label list
            [float-output "odt/floats.odt"]
            [floatfiles (labels->files labels config)]
            [floatfiles (append headers backmatter floatfiles)])
       (begin
+        ; Convert floats to single ODT
         (files->pandoc 'odt labels floatfiles float-output)
+        ; Convert each chapter to single ODT
         (let loop ([ch-ls chapters])
           (if (null? ch-ls)
               (begin
                 (delete-file endnotefile)
-                #f)
+                #f) ; return here
 
               (let* ([chapter (car ch-ls)]
                      [outfile (basename chapter)]
@@ -147,33 +155,39 @@
 
 (define make-pdf-odt
   (lambda (chapters)
+    "Convert list of markdown files CHAPTERS to single PDF and to separate ODT
+    files per chapter"
     (let* ([config "config/xref-labels.scm"]
            [config (call-with-input-file config read)]
            [endnotefile "input.ent"]
            [endnotes (touch-file endnotefile)]
            [backmatter '("chapters/floats.md")]
            
+           ; Get labels from full text of all chapters 
+           ; Assemble floats in order from label list
            [pdf-headers '("config/pdf.yaml" "chapters/head.yaml")]
            [pdf-outfile "pdf/all.pdf"]
-          
            [pdf-files (append pdf-headers chapters backmatter)]
            [labels (make-labels pdf-files '())]
            [floatfiles (labels->files labels config)]
-          
+         
+           ; Process all files as single PDF
            [pdf-files (append pdf-files floatfiles)]
-           [make-pdfs (files->pandoc 'pdf labels pdf-files pdf-outfile)]
-          
+           [make-pdf (files->pandoc 'pdf labels pdf-files pdf-outfile)]
+           
+           ; Create single ODT for float files in order
            [odt-headers '("config/odt.yaml" "chapters/head.yaml")]
            [odt-float-output "odt/floats.odt"]
            [odt-floatfiles (append odt-headers backmatter floatfiles)]
            [make-odt-floats (files->pandoc 'odt labels 
-                                           odt-floatfiles odt-float-output)]
+                                           odt-floatfiles odt-float-output)])
 
+      ; Make single ODT for each chapter
       (let make-odts ([ls chapters])
         (if (null? ls)
             (begin
               (delete-file endnotefile)
-              #f) ; return
+              #f) ; return here
             (let* ([chapter (car ls)]
                    [outfile (basename chapter)]
                    [outfile (string-append "odt/" (swap-ext outfile ".odt"))]
